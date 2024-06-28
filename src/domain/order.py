b8a6f2c24198edfa857d9f16b2b9ee4ee00d36b0
Julien VITTE <julien.vitte@insidegroup.fr>
2024-06-28 16:38:59 +0200

    refacto: removes setters in order

--- src/domain/order.py
+++ src/domain/order.py
@@ -5,24 +5,44 @@ from .product import Product
 
 
 class Order:
-    id: int = 0
-    total: Price
-    currency: str = 'EUR'
-    items: list[OrderItem]
-    tax: Price
+    _id: int = 0
+    _total: Price
+    _currency: str = 'EUR'
+    _items: list[OrderItem]
+    _tax: Price
     status: OrderStatus
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def tax(self) -> Price:
+        return self._tax
+
+    @property
+    def total(self) -> Price:
+        return self._total
+
+    @property
+    def items(self) -> [OrderItem]:
+        return self._items
+
+    @property
+    def currency(self) -> str:
+        return self._currency
 
     def __init__(self, id: int = 0, currency: str = 'EUR', status: OrderStatus = OrderStatus.CREATED) -> None:
         super().__init__()
         self.status = status
-        self.tax = Price(0, currency)
-        self.items = []
-        self.currency = currency
-        self.total = Price(0, currency)
-        self.id = id
+        self._tax = Price(0, currency)
+        self._items = []
+        self._currency = currency
+        self._total = Price(0, currency)
+        self._id = id
 
     def add_item_for(self, quantity: int, product: Product):
         order_item = OrderItem(product=product, quantity=quantity)
-        self.items.append(order_item)
-        self.total = self.total.add(order_item.taxed_amount)
-        self.tax = self.tax.add(order_item.tax)
+        self._items.append(order_item)
+        self._total = self.total.add(order_item.taxed_amount)
+        self._tax = self.tax.add(order_item.tax)
