8f608122362408b5ce14856c673d4e092c45cd57
Julien VITTE <julien.vitte@insidegroup.fr>
2024-06-28 16:38:46 +0200

    refacto: removes setters in order item

--- src/domain/order_item.py
+++ src/domain/order_item.py
@@ -3,15 +3,31 @@ from .product import Product
 
 
 class OrderItem:
-    product: Product
-    quantity: int
-    taxed_amount: Price
-    tax: Price
+    _product: Product
+    _quantity: int
+    _taxed_amount: Price
+    _tax: Price
+
+    @property
+    def product(self) -> Product:
+        return self._product
+
+    @property
+    def quantity(self) -> int:
+        return self._quantity
+
+    @property
+    def tax(self) -> Price:
+        return self._tax
+
+    @property
+    def taxed_amount(self) -> Price:
+        return self._taxed_amount
 
     def __init__(self, product: Product, quantity: int) -> None:
         super().__init__()
         unitary_tax = product.price.percent(product.category.tax_percentage).round()
-        self.tax = unitary_tax.multiply(by=quantity)
-        self.taxed_amount = unitary_tax.add(product.price).round().multiply(by=quantity).round()
-        self.quantity = quantity
-        self.product = product
+        self._tax = unitary_tax.multiply(by=quantity)
+        self._taxed_amount = unitary_tax.add(product.price).round().multiply(by=quantity).round()
+        self._quantity = quantity
+        self._product = product
