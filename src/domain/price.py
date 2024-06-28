686e890460f458f7c9aaf466e9087227452a44bc
Julien VITTE <julien.vitte@insidegroup.fr>
2024-06-28 16:14:22 +0200

    feat: adds guards for price rules

--- src/domain/price.py
+++ src/domain/price.py
@@ -8,8 +8,13 @@ class Price:
     amount: float
     currency: str
 
+    def __post_init__(self):
+        self._value_must_be_positive()
+        self._value_must_be_finite()
+        pass
+
     def add(self, price: Self) -> Self:
-        # self._currencies_must_be_the_same(price)
+        self._currencies_must_be_the_same(price)
         return Price(self.amount + price.amount, self.currency)
 
     def percent(self, percentage: float) -> Self:
@@ -25,3 +30,15 @@ class Price:
         if not isinstance(price, Price):
             return False
         return self.amount == price.amount and self.currency == price.currency
+
+    def _value_must_be_finite(self):
+        if self.amount == float('inf'):
+            raise ValueError('Price value must be finite')
+
+    def _currencies_must_be_the_same(self, price: Self) -> None:
+        if self.currency != price.currency:
+            raise ValueError(f'Can not add prices in %s and %s'.format(self.currency, price.currency))
+
+    def _value_must_be_positive(self) -> None:
+        if self.amount < 0:
+            raise ValueError(f"A price must be positive, {self.amount:.2f} given")
--- test/unit/domain/test_price.py
+++ test/unit/domain/test_price.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.domain.price import Price
 
 
@@ -7,12 +9,26 @@ def ten_euros():
 
 class TestPrice:
 
+    def test_must_have_positive_value(self) -> None:
+        with pytest.raises(ValueError, match='A price must be positive, -5.00 given'):
+            Price(-5, 'EUR')
+
+    def test_must_have_a_finite_value(self) -> None:
+        with pytest.raises(ValueError, match='Price value must be finite'):
+            Price(float('inf'), 'EUR')
+
     def test_can_add_to_another_price_when_currencies_are_the_same(self) -> None:
         five_euros = Price(5, 'EUR')
 
         sum = ten_euros().add(five_euros)
 
         assert sum == Price(15, 'EUR')
+
+    def test_can_not_add_price_with_different_currencies(self) -> None:
+        five_dollars = Price(5, 'USD')
+
+        with pytest.raises(ValueError):
+            ten_euros().add(five_dollars)
 
     def test_can_apply_percentage(self) -> None:
         tax = ten_euros().percent(10)
