10bd1bb0bfd5c8de7e3690f3debd836f0c20ab2d
Julien VITTE <julien.vitte@insidegroup.fr>
2024-07-08 13:52:23 +0200

    feat(state): adds each state classes

--- src/domain/order_status.py
+++ src/domain/order_status.py
@@ -13,3 +13,27 @@ class OrderState(ABC):
     @abstractmethod
     def get_status(self) -> OrderStatus:
         pass
+
+
+class Created(OrderState):
+
+    def get_status(self) -> OrderStatus:
+        return OrderStatus.CREATED
+
+
+class Approved(OrderState):
+
+    def get_status(self) -> OrderStatus:
+        return OrderStatus.APPROVED
+
+
+class Shipped(OrderState):
+
+    def get_status(self) -> OrderStatus:
+        return OrderStatus.SHIPPED
+
+
+class Rejected(OrderState):
+
+    def get_status(self) -> OrderStatus:
+        return OrderStatus.REJECTED
